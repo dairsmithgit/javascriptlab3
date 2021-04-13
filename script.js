bdfb6ac7cec177eccbaf8977473f9a064e52eeda
Dair Smith <dair.code@gmail.com>
2021-04-13 16:14:30 -0400

    add submission function

--- script.js
+++ script.js
@@ -1,26 +1,35 @@
 let submissions = [
-    {
-        name: "Jane",
-        score: 95,
-        date: "2020-01-24",
-        passed: true
-    },
-    {
-        name: "Joe",
-        score: 77,
-        date: "2018-05-14",
-        passed: true
-    },
-    {
-        name: "Jack",
-        score: 59,
-        date: "2019-07-05",
-        passed: false
-    },
-    {
-        name: "Jill",
-        score: 88,
-        date: "2020-04-22",
-        passed: true
-    }
+  {
+    name: "Jane",
+    score: 95,
+    date: "2020-01-24",
+    passed: true,
+  },
+  {
+    name: "Joe",
+    score: 77,
+    date: "2018-05-14",
+    passed: true,
+  },
+  {
+    name: "Jack",
+    score: 59,
+    date: "2019-07-05",
+    passed: false,
+  },
+  {
+    name: "Jill",
+    score: 88,
+    date: "2020-04-22",
+    passed: true,
+  },
 ];
+
+function addSubmission(array, newName, newScore, newDate) {
+  array.push({
+    name: newName,
+    score: newScore,
+    date: newDate,
+    passed: newScore >= 60 ? true : false,
+  });
+}
